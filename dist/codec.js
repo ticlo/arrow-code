9544eeb9956932413bde8721b353b649233bf05d
rinick <rinick@gmail.com>
2020-10-24 22:20:04 -0700

    bump dependencies

--- dist/codec.js
+++ dist/codec.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.decodeUint8Array = exports.encodeUint8Array = exports.decodeDate = exports.encodeDate = void 0;
 const base93_1 = require("./base93");
 function encodeDate(self) {
     return '\u001bDate:' + self.toISOString();
