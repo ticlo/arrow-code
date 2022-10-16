486d57b76a07fc0874e2de75f8f9429c699d4f40
rinick <rinick@gmail.com>
2022-10-16 16:13:27 -0700

    add es lib

--- dist/base93.d.ts
+++
@@ -1,4 +0,0 @@
-export default class Base93 {
-    static encode(data: number[] | Uint8Array, prefix?: string): string;
-    static decode(str: string, offset?: number, length?: number): number[];
-}
--- dist/codec.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function encodeDate(self: Date): string;
-export declare function decodeDate(str: string): Date;
-export declare function encodeUint8Array(self: Uint8Array): string;
-export declare function decodeUint8Array(str: string): Uint8Array;
